214d54813c35f7dc66de3be7ffc09e907330f04e
0xol <86998622+0xol@users.noreply.github.com>
2022-09-26 14:26:14 +1000

    test

--- main.py
+++ main.py
@@ -1,50 +1,7 @@
-'''
-
-import csv
-import time
-
-Descriptions = {"Pur Accessories", "Pur Homewares", "Pur Manchester", "Pur Sundries", "Mens Wear", "Childrens Wear", "Accessories", "Book", "Electrical", "Homewares", "Manchester", "Sundries", "Women wear", "Discount Allowed", "Donation", "Discount Allowed", "COGS", "30500", "Clearing - Bank", "Cash Total", " EFTPOS Total"}
-
-allStores = set(())
-
-salesRepFile = open("salesRep.csv", newline = '')
-vendJournalFile = open("vendJournal.csv", newline = '')
-
-salesRep = csv.reader(salesRepFile)
-vendJournal = csv.reader(vendJournalFile)
-
-for row in salesRep:
-    column = list(row)
-    allStores.add(column[0])
-
-for row in vendJournal:
-    column = list(row)
-    allStores.add(column[2])
-
-#weird bug with csv functions
-
-allStores.remove('')
-allStores.remove('Revenue')
-allStores.remove('OutletName')
-allStores.remove('Margin (%)')
-allStores.remove('Cost of Goods Sold')
-allStores.remove('Revenue (Incl. Tax)')
-allStores.remove('Tax')
-
-print("TEST")
-
-for row in vendJournal:
-    column = list(row)
-    print(column)
-
-print("TEST")
-
-print(allStores)
-
-'''
-
 import pandas as pd
 
-df = pd.read_csv('salesRep.csv')
+salesRep = pd.read_csv("salesRep.csv")
+vendJournal = pd.read_csv("vendJournal.csv")
 
-print(df) 
+print(vendJournal)
+print(salesRep)
