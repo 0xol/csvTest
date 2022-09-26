062448aba8d4788bca791361d9cd9b3b4435549e
0xol <86998622+0xol@users.noreply.github.com>
2022-09-26 14:42:57 +1000

    test :)

--- main.py
+++ main.py
@@ -3,5 +3,4 @@ import pandas as pd
 salesRep = pd.read_csv("salesRep.csv")
 vendJournal = pd.read_csv("vendJournal.csv")
 
-print(vendJournal)
-print(salesRep[['Tax']])
+print(vendJournal.loc[df["OutletName"] == "Windsor"])
