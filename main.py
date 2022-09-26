8704b8e56d50f0d90ba297760fa20b0bc670d8ca
0xol <86998622+0xol@users.noreply.github.com>
2022-09-26 10:07:53 +1000

    test

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import csv
 import time
 
+Descriptions = {"Pur Accessories", "Pur Homewares", "Pur Manchester", "Pur Sundries", "Mens Wear", "Childrens Wear", "Accessories", "Book", "Electrical", "Homewares", "Manchester", "Sundries", "Women wear", "Discount Allowed", "Donation", "Discount Allowed", "COGS", "30500", "Clearing - Bank", "Cash Total", " EFTPOS Total"}
+
+allStores = {}
+
 salesRepFile = open("salesRep.csv", newline = '')
 vendJournalFile = open("vendJournal.csv", newline = '')
 
@@ -8,7 +12,8 @@ salesRep = csv.reader(salesRepFile)
 vendJournal = csv.reader(vendJournalFile)
 
 for row in salesRep:
-    print(row)
+    column = list(row)
+    print(column[0])
 
 for row in vendJournal:
     print(row)
